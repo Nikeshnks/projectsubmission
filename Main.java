0125366be5a26554faef0cd0e79ed83eeeb3fc98
Nikeshnks <nikesh.sapkota10000@gmail.com>
2026-04-11 00:08:52 +0545

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,43 +1,27 @@
 package application;
-
+	
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
 
 public class Main extends Application {
-
-    @Override
-    public void start(Stage stage) {
-        Label title = new Label("Online Library Management System");
-
-        TextField username = new TextField();
-        username.setPromptText("Username");
-
-        PasswordField password = new PasswordField();
-        password.setPromptText("Password");
-
-        Button loginBtn = new Button("Login");
-        Label message = new Label();
-
-        loginBtn.setOnAction(e -> {
-            if(username.getText().equals("admin") && password.getText().equals("admin")) {
-                message.setText("Login Successful!");
-            } else {
-                message.setText("Invalid Credentials!");
-            }
-        });
-
-        VBox root = new VBox(10, title, username, password, loginBtn, message);
-        Scene scene = new Scene(root, 400, 300);
-
-        stage.setTitle("OLMS");
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
+
